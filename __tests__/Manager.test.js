2ff0e44644f57ca8a6723af18a14b73b591857a6
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-06-27 13:00:05 -0400

    created a Manager object that extends the Employee to include its properties and menthods.

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,15 @@
 const Manager = require('../lib/Manager');
 
+test('creates an Manager object',()=>{
+    const manager = new Manager('Dave',12345,'Dave@gmail.com',21);
+    
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test("gets manager's role",()=>{
+    const manager = new Manager('Dave',12345,'Dave@gmail.com',21);
+    expect(manager.getRole()).toBe('Manager');
+})
